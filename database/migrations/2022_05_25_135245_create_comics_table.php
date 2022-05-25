094c10141b2aa107f843682b827a47ceb9082727
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-05-25 16:24:57 +0200

    database loaded

--- database/migrations/2022_05_25_135245_create_comics_table.php
+++ database/migrations/2022_05_25_135245_create_comics_table.php
@@ -17,7 +17,7 @@ class CreateComicsTable extends Migration
             $table->id();
 
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->string('thumb');
             $table->float('price');
             $table->string('series');
